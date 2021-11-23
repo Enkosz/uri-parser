e3e6f72dda66484a558bb1059df7b3c764bdb2cd
Simone Giannino <simonegioele92@gmail.com>
2021-11-23 11:43:38 +0100

    Test if

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,14 +1,12 @@
 % uri_parse/2
 
-uri_parse(URIString, uri(Schema, UserInfo, Authority, RestPath)) :-
+uri_parse(URIString, uri(Schema, Authority, RestPath)) :-
     string(URIString),
     string_chars(URIString, Chars),
-    uri_schema(Chars, SchemaList, ['/', '/' | RestUserInfo]),
-    uri_userinfo(RestUserInfo, UserInfoList, RestAuthority),
+    uri_schema(Chars, SchemaList, RestAuthority),
     uri_authority(RestAuthority, AuthorityList, RestPath),
     string_chars(Schema, SchemaList),
-    string_chars(Authority, AuthorityList),
-    string_chars(UserInfo, UserInfoList).
+    string_chars(Authority, AuthorityList).
 
 % uri_schema/3
 
@@ -18,8 +16,16 @@ uri_schema([X | Xs], [X | Ys],  Rest) :-
 
 % uri_authority/3
 
-uri_authority(Host, ParsedHost, Rest) :-
-    uri_host(Host, ParsedHost, Rest).
+uri_authority(['/', '/' | Host], ParsedAuth, Rest) :-
+    (uri_userinfo(Host, ParsedInfo, RestInfo) -> 
+    true
+    ; ParsedInfo = []
+    ),
+    (uri_host(RestInfo, ParsedHost, RestHost) -> 
+    true
+    ; ParsedHost = []
+    ),
+    append(ParsedInfo, ParsedHost, ParsedAuth).
 
 uri_host([], [], _) :- !. % nel caso finisse qui URI
 uri_host(['/' | Rest], [], Rest) :- !.
