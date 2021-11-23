20895c2db80d52d42adfe2aeaae29158da2ad854
Simone Giannino <simonegioele92@gmail.com>
2021-11-23 11:17:55 +0100

    Aggiunto parsing UserInfo

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,11 +1,14 @@
 % uri_parse/2
 
-uri_parse(URIString, uri(Schema, Authority, RestPath)) :-
+uri_parse(URIString, uri(Schema, UserInfo, Authority, RestPath)) :-
     string(URIString),
     string_chars(URIString, Chars),
-    uri_schema(Chars, SchemaList, Rest),
-    uri_authority(Rest, Authority, RestPath),
-    string_chars(Schema, SchemaList).
+    uri_schema(Chars, SchemaList, ['/', '/' | RestUserInfo]),
+    uri_userinfo(RestUserInfo, UserInfoList, RestAuthority),
+    uri_authority(RestAuthority, AuthorityList, RestPath),
+    string_chars(Schema, SchemaList),
+    string_chars(Authority, AuthorityList),
+    string_chars(UserInfo, UserInfoList).
 
 % uri_schema/3
 
@@ -15,10 +18,15 @@ uri_schema([X | Xs], [X | Ys],  Rest) :-
 
 % uri_authority/3
 
-uri_authority(['/', '/' | Host], ParsedHost, Rest) :-
+uri_authority(Host, ParsedHost, Rest) :-
     uri_host(Host, ParsedHost, Rest).
 
 uri_host([], [], _) :- !. % nel caso finisse qui URI
 uri_host(['/' | Rest], [], Rest) :- !.
 uri_host([X | Xs], [X | Ys],  Rest) :- 
     uri_host(Xs, Ys, Rest).
+
+% uri_userinfo/3
+uri_userinfo(['@' | Rest], [], Rest) :- !.
+uri_userinfo([X | Xs], [X | Ys],  Rest) :- 
+    uri_userinfo(Xs, Ys, Rest).
