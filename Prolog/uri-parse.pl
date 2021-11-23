4d56ca2ce15603d2980dd67b4ac9eff28247fcca
Lorenzo <lorenzo.avallone01@gmail.com>
2021-11-23 01:15:50 +0100

    Modificato regola uri_host per recuperare un generico Host

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,18 +1,24 @@
 % uri_parse/2
 
-uri_parse(URIString, uri(Schema, Authority, Rest)) :-
+uri_parse(URIString, uri(Schema, Authority, RestPath)) :-
     string(URIString),
     string_chars(URIString, Chars),
     uri_schema(Chars, SchemaList, Rest),
-    uri_authority(Rest, Authority, Rest),
+    uri_authority(Rest, Authority, RestPath),
     string_chars(Schema, SchemaList).
+
+% uri_schema/3
 
 uri_schema([':' | Rest], [], Rest) :- !.
 uri_schema([X | Xs], [X | Ys],  Rest) :-
     uri_schema(Xs, Ys, Rest).
 
+% uri_authority/3
+
 uri_authority(['/', '/' | Host], ParsedHost, Rest) :-
     uri_host(Host, ParsedHost, Rest).
 
-uri_host([], [], []) :- !.
-uri_host(['/' | L], [], ['/' | L]) :- !.
+uri_host([], [], _) :- !. % nel caso finisse qui URI
+uri_host(['/' | Rest], [], Rest) :- !.
+uri_host([X | Xs], [X | Ys],  Rest) :- 
+    uri_host(Xs, Ys, Rest).
