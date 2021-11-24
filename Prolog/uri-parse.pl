7d378331686f5e438b502f4bd8ac62386bd4ab7c
Lorenzo <lorenzo.avallone01@gmail.com>
2021-11-24 16:55:08 +0100

    First Commit DCG

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,24 +1,3 @@
-% uri_parse/2
-
-uri_parse(URIString, uri(Schema, Authority, RestPath)) :-
-    string(URIString),
-    string_chars(URIString, Chars),
-    uri_schema(Chars, SchemaList, Rest),
-    uri_authority(Rest, Authority, RestPath),
-    string_chars(Schema, SchemaList).
-
-% uri_schema/3
-
-uri_schema([':' | Rest], [], Rest) :- !.
-uri_schema([X | Xs], [X | Ys],  Rest) :-
-    uri_schema(Xs, Ys, Rest).
-
-% uri_authority/3
-
-uri_authority(['/', '/' | Host], ParsedHost, Rest) :-
-    uri_host(Host, ParsedHost, Rest).
-
-uri_host([], [], _) :- !. % nel caso finisse qui URI
-uri_host(['/' | Rest], [], Rest) :- !.
-uri_host([X | Xs], [X | Ys],  Rest) :- 
-    uri_host(Xs, Ys, Rest).
+% 866163 Avallone Lorenzo
+% 872783 Giannino Simone
+% 866147 Biotto Simone
