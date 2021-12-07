98b98d160eb0ea98480bc350c67ba7488f52c0b0
Simone Giannino <simonegioele92@gmail.com>
2021-12-07 13:19:42 +0100

    mi viene da piangere

--- Lisp/test.lisp
+++ Lisp/test.lisp
@@ -1,26 +1,76 @@
-(defclass Scheme ()
-    (value)
-)
+#| (defun identificator% (lista delimitator)
+  (cond (
+    ((null lista) nil)
+    ((eq (car lista) delimitator) (values nil (cdr lista)))
+    (T (multiple-value-bind (left right)
+      (identificator% (cdr lista) delimitator)
+      ()
+    ))
+  ))
+) |#
 
-(defun identificator (InputList &optional Delimitator)
-  (if (null InputList) nil 
-    (if (eq (first InputList) Delimitator) (values nil (cdr InputList))
-    (multiple-value-bind (result rest)
-      (identificator (cdr InputList) Delimitator)
-      (values (cons (car InputList) result) rest)
+(defun without-last(l)
+    (reverse (cdr (reverse l)))
+    )
+
+(defun identificator% (list &optional del)
+  (cond ((null list) nil)
+        ((not (member del list)) (values nil list))
+        ((eq (first list) del) (values nil (cdr list)))
+        (T (multiple-value-bind (result rest)
+          (identificator% (cdr list) del); expr
+          (values (cons (car list) result) rest); body
+          ))
     )
   )
-))
 
-(defun scheme (InputList)
-  (if (null InputList) nil 
-    (multiple-value-bind (scheme rest)
-      (identificator InputList #\:)
-      (values scheme rest)
-    )
+; "abc" c -> "bc" c -> 
+; identificator% "abc" "b" -> (a) (c)
+; identificator% nil "b" -> nil
+; identificator% "abc" "d" -> nil
+
+(defun identificator (list delimitator)
+  (let ((parse (multiple-value-list (identificator% list delimitator))))
+    (if (first parse)
+        (values-list parse)
+      (values nil (second parse)))))
+
+(defun scheme (list)
+  (multiple-value-bind (schema rest)
+    (identificator list #\:)
+    (values (cons 'uri-scheme schema) rest)
+  )
+)
+
+(defun userinfo (list)
+  (multiple-value-bind (schema rest)
+    (identificator list #\@)
+    (values (cons 'uri-userinfo schema) rest)
+  )
+)
+
+(defun host (list)
+(multiple-value-bind (schema rest)
+    (identificator list #\:)
+    (values (cons 'uri-host schema) rest)
+  )
+)
+
+(defun authority (list)
+  (cond 
+    ((and (eq (first list) #\/) (eq (second list) #\/)) 
+      (let* (
+        (parsedUserInfo (multiple-value-list (userinfo list)))
+        (parsedHost (multiple-value-list (host (second parsedUserInfo)))))
+        (list (first parsedUserInfo) (first parsedHost))
+      ))
   )
 )
 
 (defun parse (InputString)
-  (scheme (coerce InputString 'list))
+  (let* (
+    (parsedSchema (multiple-value-list (scheme (coerce InputString 'list))))
+    (parsedAuthority (multiple-value-list (authority (second parsedSchema)))))
+    (list (first parsedSchema) parsedAuthority)
+  )
 )
