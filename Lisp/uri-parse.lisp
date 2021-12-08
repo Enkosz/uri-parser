ef43e0b55691b2d5abf2a384fd88f35ad3c47b47
Lorenzo Avallone <lorenzo.avallone01@gmail.com>
2021-12-08 18:50:06 +0100

    Scherzetto

--- Lisp/uri-parse.lisp
+++ Lisp/uri-parse.lisp
@@ -1,51 +1,132 @@
 ; Definizione delle Classi
-  (:constructor create-uri-scheme (value)))
-  value
-)
-
-(defstruct (uri_userinfo
-  (:constructor create-uri-userinfo (value)))
-  value
-)
-
-(defstruct (uri_host
-  (:constructor create-uri-host (value)))
-  value
-)
-
-(defstruct (uri_port
-  (:constructor create-uri-port (value)))
-  value
-)
-
-(defstruct (uri_authority 
-  (:constructor create-uri-authority (uri_userinfo uri_host uri_port)))
-  uri_userinfo
-  uri_host
-  uri_port
-)
-
-(defstruct (uri_component
-  (:constructor create-uri-component (
-    uri_scheme
-    uri_authority
-    uri_path
-    uri_query
-    uri_fragment
-  )))
-  uri_scheme
-  uri_authority
-  uri_path
-  uri_query
-  uri_fragment
-)
-
 (defclass schema ()
   ((value
     :initarg :value
     :accessor value)
   )
 )
+
+(defclass userinfo ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass host ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass port ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass path ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass query ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass fragment ()
+  ((value
+    :initarg :value
+    :accessor value)
+  )
+)
+
+(defclass authority ()
+  (
+    (userinfo
+     :initarg :userinfo
+     :initform nil
+     :accessor get-userinfo)
+    (host
+     :initarg :host
+     :initform nil
+     :accessor get-host)
+    (port
+     :initarg :port
+     :initform nil
+     :accessor get-port)
+   )
+)
+
+(defclass uri-structure ()
+ (
+  (schema
+   :initarg :schema
+   :accessor get-schema
+   )
+  (authority
+   :initarg :authority
+   :initform nil
+   :accessor get-authority
+   )
+  (path
+   :initarg :path
+   :initform nil
+   :accessor get-path
+   )
+  (query
+   :initarg :query
+   :initform nil
+   :accessor get-query
+   )
+  (fragment
+   :initarg :fragment
+   :initform nil
+   :accessor get-fragment
+   )
+  )
+ )
+
+(defun uri-scheme (uri-struct)
+  (value (get-schema uri-struct))
+ )
+
+(defun uri-userinfo (uri-struct)
+  (cond ((null (get-userinfo (get-authority uri-struct))) nil)
+        (T (value (get-userinfo (get-authority uri-struct)))))
+ )
+
+(defun uri-host (uri-struct)
+  (cond ((null (get-host (get-authority uri-struct))) nil)
+        (T (value (get-host (get-authority uri-struct)))))
+ )
+
+(defun uri-port (uri-struct)
+  (cond ((null (get-port (get-authority uri-struct))) nil)
+        (T (value (get-port (get-authority uri-struct)))))
+ )
+
+(defun uri-path (uri-struct)
+  (cond ((null (get-path uri-struct)) nil)
+        (T (value (get-path uri-struct))))
+ )
+
+(defun uri-query (uri-struct)
+  (cond ((null (get-query uri-struct)) nil)
+        (T (value (get-query uri-struct))))
+ )
+
+(defun uri-fragment (uri-struct)
+  (cond ((null (get-fragment uri-struct)) nil)
+        (T (value (get-fragment uri-struct))))
+ )
 
 (defun identificator% (list &optional delimitators accumulator)
   (when list
@@ -64,57 +145,90 @@
 (defun parse-scheme (list)
   (multiple-value-bind (parsed remaining)
     (identificator list '(#\:))
-    (values (create-uri-scheme (coerce parsed 'string)) (rest remaining))
+    (values (make-instance 'schema :value (coerce parsed 'string)) (rest remaining))
   )
 )
 
 (defun parse-userinfo (list)
   (multiple-value-bind (parsed remaining)
     (identificator list '(#\@))
-    (values (create-uri-userinfo (coerce parsed 'string)) remaining)
-  )
-)
+    (cond ((null parsed) 
+            (values nil remaining)
+           )
+          (T (values 
+              (make-instance 'userinfo :value (coerce parsed 'string)) 
+              (cdr remaining))))))
 
 (defun parse-host (list)
   (multiple-value-bind (parsed remaining)
       (identificator list '(#\: #\/ eof))
-      (values (create-uri-host (coerce parsed 'string)) remaining)
+      (cond ((null parsed) 
+            (values nil remaining)
+           )
+          (T (values 
+              (make-instance 'host :value (coerce parsed 'string)) 
+              remaining))
+     )
     )
 )
 
 (defun parse-port (list)
    (if (not (eq (first list) #\:))
-      (values (list 'uri-port) list)
+      (values nil list)
       (multiple-value-bind (parsed remaining)
         (identificator list '(#\/ eof))
-        (values (create-uri-port (coerce parsed 'string)) remaining)
+        (cond ((null parsed) 
+            (values nil remaining)
+           )
+          ((every #'digit-char-p (coerce parsed 'string)) (values 
+              (make-instance 'port :value (coerce parsed 'string)) 
+              (cdr remaining)))
+        )
       )
   )
 )
 
 (defun parse-path (list)
-  (multiple-value-bind (parsed rest)
+  (multiple-value-bind (parsed remaining)
     (identificator list '(#\? #\# eof))
-    (values (list 'uri-path parsed) rest)
+    (cond ((null parsed) 
+            (values nil remaining)
+           )
+          (T (values 
+              (make-instance 'path :value (coerce parsed 'string)) 
+              remaining))
+     )
   )
 )
 
 (defun parse-query (list)
   (if (not (eq (first list) #\?))
-    (values (list 'uri-query) list)
-    (multiple-value-bind (parsed rest)
-      (identificator list '(#\# eof))
-      (values (list 'uri-query parsed) rest)
+    (values nil list)
+    (multiple-value-bind (parsed remaining)
+      (identificator (cdr list) '(#\# eof))
+      (cond ((null parsed) 
+            (values nil remaining)
+           )
+          (T (values 
+              (make-instance 'userinfo :value (coerce parsed 'string)) 
+              remaining))
+     )
     )
   )
 )
 
 (defun parse-fragment (list)
   (if (not (eq (first list) #\#))
-      (values (list 'uri-fragment) list)
-      (multiple-value-bind (parsed rest)
-        (identificator list '(eof))
-        (values (list 'uri-fragment parsed) rest)
+      (values nil list)
+      (multiple-value-bind (parsed remaining)
+        (identificator (cdr list) '(eof))
+        (cond ((null parsed) 
+            (values nil remaining)
+           )
+          (T (values 
+              (make-instance 'userinfo :value (coerce parsed 'string)) 
+              (cdr remaining)))
+     )
     )
   )
 )
@@ -125,12 +239,17 @@
     (parsed_userinfo (multiple-value-list (parse-userinfo URIStringList)))
     (parsed_host (multiple-value-list (parse-host (second parsed_userinfo))))
     (parsed_port (multiple-value-list (parse-port (second parsed_host)))))
-    (values (create-uri-authority (first parsed_userinfo) (first parsed_host) (first parsed_port)) (second parsed_port))
+    (values 
+     (make-instance 'authority 
+      :userinfo (first parsed_userinfo) 
+      :host (first parsed_host) 
+      :port (first parsed_port)) 
+     (second parsed_port))
   )
 )
 
 ; scheme ‘:’ authorithy [‘/’ [path] [‘?’ query] [‘#’ fragment]]
-(defun parse-default-uri-type (URIStringList)
+(defun parse-default-uri (URIStringList)
   (let* (
     (parsed_authority (multiple-value-list (parse-authority URIStringList)))
     (parsed_path (multiple-value-list (parse-path (second parsed_authority))))
@@ -147,7 +266,7 @@
 
 ; scheme ‘:’ [‘/’] [path] [‘?’ query] [‘#’ fragment]
 ; TODO Gestire il slash opzionale
-(defun parse-resource-uri-type (URIStringList)
+(defun parse-resource-uri (URIStringList)
   (let* (
     (parsed_path (multiple-value-list (parse-path URIStringList)))
     (parsed_query (multiple-value-list (parse-query (second parsed_path))))
@@ -168,11 +287,16 @@
     (cond
       ((and
         (char= (first (second parsed_scheme)) #\/)
-        (char= (second (second parsed_scheme)) #\/)) 
-        (append (first parsed_scheme) (parse-default-uri-type (cdr (cdr (second parsed_scheme)))))
-      )
+        (char= (second (second parsed_scheme)) #\/))
+        (let 
+          ((otherComp (parse-default-uri (cdr (cdr (second parsed_scheme))))))
+          (make-instance 'uri-structure :schema (first parsed_scheme) 
+                                        :authority (first otherComp)
+                                        :path (second otherComp)
+                                        :query (third otherComp)
+                                        :fragment (fourth otherComp))))
       (T 
-        (append (first parsed_scheme) (parse-resource-uri-type (second parsed_scheme)))
+        (append (first parsed_scheme) (parse-resource-uri (second parsed_scheme)))
       )
     )
   )
@@ -181,13 +305,7 @@
 ; Utility interna, aggiunge EOF alla fine della lista come delimitatore
 ; Richiama una funzione che gestisce i vari tipi di URI
 (defun uri-parse_ (URIStringList)
-  (let (
-    (parsed_input (append URIStringList '(eof))))
-    (let (
-        (parsed_uri (multiple-value-list (parse-uri-type parsed_input))))
-        (create-uri-component 'uri-component parsed_uri)
-    )
-  )
+  (parse-uri-type (append URIStringList '(eof)))
 )
 
 ; Funzione principale, converta la stringa in Input come una lista di caratteri
