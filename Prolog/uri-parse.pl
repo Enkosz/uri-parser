d7f158c986bbbc361de36a4e595e84e0431b1cbf
Zuppa <smn01b@gmail.com>
2021-12-15 22:46:19 +0100

    Sistemato uri_display, brutto ma funzionante

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -13,22 +13,23 @@ uri_display(URIStruct) :-
 
 uri_display(uri(Scheme, UserInfo, Host, Port, Path, Query, Frag), Stream) :-
     is_stream(Stream),
-    uri_parse(URIString,
-	      uri(Scheme, UserInfo, Host, Port, Path, Query, Fragment)),
-    format(Stream,
-	   'Scheme: ~w ~n
-	    Userinfo: ~w ~n
-	    Host: ~w ~n
-	    Port: ~w ~n
-	    Path: ~w ~n
-	    Query: ~w ~n
-	    Fragment: ~w',
-	   [Scheme, UserInfo, Host, Port, Path, Query, Fragment]).
+    format(Stream,'Scheme:      ~w ~n', [Scheme]),
+    format(Stream,'UserInfo:    ~w ~n', [UserInfo]),
+    format(Stream,'Host:        ~w ~n', [Host]),
+    format(Stream,'Port:        ~w ~n', [Port]),
+    format(Stream,'Path:        ~w ~n', [Path]),
+    format(Stream,'Query:       ~w ~n', [Query]),
+    format(Stream,'Fragment:    ~w', [Frag]),
+    !.
 
+uri_display(false, Stream) :-
+    is_stream(Stream),
+    format(Stream, 'Uri invalid'),
+    !.
 %------------------------------------------------------------------------------
 
 % uri_parse
-uri_parse(URIString, uri(Scheme, UserInfo, Host, Port, Path, Query, Fragment)) :-
+uri_parse(URIString, uri(Scheme, UserInfo, Host, Port, Path, Query, Frag)) :-
     string_chars(URIString, URIChars),
     uri_parse_(URIChars,
                uri(components(
@@ -38,7 +39,7 @@ uri_parse(URIString, uri(Scheme, UserInfo, Host, Port, Path, Query, Fragment)) :
 		       port(Port),
 		       path(Path), 
 		       query(Query), 
-		       fragment(Fragment)
+		       fragment(Frag)
 		   ))).
 
 uri_parse_(URIString, uri(URI)) :-
